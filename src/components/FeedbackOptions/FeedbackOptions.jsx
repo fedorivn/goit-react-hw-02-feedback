c94c340ea0cd8f5820b36cf04c47307985007915
Your Name <wtm@hotmail.com.au>
2023-02-16 18:56:33 +0200

    mf

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {Container,  Button, ButtonsList } from './FeedbackOptions.styled'
+
+export const FeedbackOptions = ({ options, onClickHendler }) => {
+  return (
+   <Container>
+    <ButtonsList>
+      {options.map(option => (
+        <Button key={option} onClick={() => onClickHendler(option)}>
+          {option}
+        </Button>
+      ))}
+    </ButtonsList >
+    </Container>
+  );
+};
+FeedbackOptions.propTypes = {
+ onClickHendler: PropTypes.func
+};
